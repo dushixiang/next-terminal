a2b30517612124a834258bbbea2c655c6f70a4a6
dushixiang <helloworld1024@foxmail.com>
2021-01-29 20:04:04 +0800

    修复username唯一索引导致初始化数据库失败的问题

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -13,7 +13,7 @@ const (
 
 type User struct {
 	ID         string         `gorm:"primary_key" json:"id"`
-	Username   string         `gorm:"index:unique" json:"username"`
+	Username   string         `gorm:"uniqueIndex" json:"username"`
 	Password   string         `json:"password"`
 	Nickname   string         `json:"nickname"`
 	TOTPSecret string         `json:"-"`
