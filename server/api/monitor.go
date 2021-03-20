06abca6925c03960141a4023a67fc1fc40738181
dushixiang <helloworld1024@foxmail.com>
2021-03-20 21:21:13 +0800

    修改日志根据配置文件判断是否要打印请求日志

---
+++ server/api/monitor.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+//
+func MonitorEndpoint(c echo.Context) (err error) {
+	//ws, err := UpGrader.Upgrade(c.Response().Writer, c.Request(), nil)
+	//if err != nil {
+	//	log.Errorf("升级为WebSocket协议失败：%v", err.Error())
+	//	return err
+	//}
+	return
+
+}
