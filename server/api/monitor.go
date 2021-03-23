36e4823c201ff2fb6fcd766d607face6013b7f72
neverteaser <neverteaser@hotmail.com>
2021-03-23 19:06:50 +0800

    feat(monitor):完善监控模块(base/docker)

--- server/api/monitor.go
+++ server/api/monitor.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"next-terminal/pkg/log"
 	"next-terminal/pkg/term"
@@ -10,19 +15,46 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/ssh"
 )
 
 const (
-	DockerCMD = "docker stats --no-stream --format \"table{{.Container}}\\t{{.CPUPerc}}\\t{{.MemUsage}}\\t{{.Name}}\""
+	DockerCMD = "docker stats --no-stream --format \"table{{.Container}}[,]{{.CPUPerc}}[,]{{.MemUsage}}[,]{{.Name}}\""
 
-	TopCMD = `top -n 1 `
+	TopCMD = "top -n 1 -b -s"
+
+	NetWorkCMD = "/sbin/ifconfig eth0 |grep bytes; sleep 1s; /sbin/ifconfig eth0 | grep bytes"
 )
 
-// TODO 定义响应
 type MonitorInfo struct {
-	Memory  interface{}  `json:"memory"`
-	Docker  []DockerInfo `json:"docker"`
-	NetWork interface{}  `json:"net_work"`
+	BaseInfo BaseInfo     `json:"base_info"`
+	Docker   []DockerInfo `json:"docker"`
+	NetWork  NewWork      `json:"net_work"`
+}
+
+type NewWork struct {
+	RxTotal          int64 `json:"rx_total"`
+	TxTotal          int64 `json:"tx_total"`
+	RxSpeedPreSecond int64 `json:"rx_speed_pre_second"`
+	TxSpeedPreSecond int64 `json:"tx_speed_pre_second"`
+}
+
+type BaseInfo struct {
+	// uptime
+	Uptime     string `json:"uptime"`
+	OnlineUser int    `json:"online_user"`
+	// memory
+	Total int64 `json:"total"`
+	Free  int64 `json:"free"`
+	Used  int64 `json:"used"`
+	Cache int64 `json:"cache"`
+	// cpu
+	TotalUse float64 `json:"total_use"`
+	SysUse   float64 `json:"sys_use"`
+	UserUse  float64 `json:"user_use"`
+	IOWait   float64 `json:"io_wait"`
+	Steal    float64 `json:"steal"`
 }
 
 type DockerInfo struct {
@@ -30,6 +62,141 @@ type DockerInfo struct {
 	ContainerName string `json:"container_name"`
 	CpuPre        string `json:"cpu_pre"`
 	MemUsageLimit string `json:"mem_usage_limit"`
+}
+
+// TODO NETWork
+func GetNetWorkInfo(client *ssh.Client) (ret NewWork, err error) {
+	return
+}
+
+func GetDockerInfo(client *ssh.Client) (ret []DockerInfo, err error) {
+	session, err := client.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	stdOut, _ := session.StdoutPipe()
+	err = session.Run(DockerCMD)
+	if err != nil {
+		return nil, err
+	}
+	res, _ := ioutil.ReadAll(stdOut)
+	for _, line := range strings.Split(string(res), "\n")[1:] {
+		if line == "" {
+			continue
+		}
+		dockerStr := strings.Split(line, "[,]")
+		if len(dockerStr) != 4 {
+			log.Error(errors.New(" dockerStr format error"), line)
+			continue
+		}
+		ret = append(ret, DockerInfo{
+			ContainerID:   dockerStr[0],
+			ContainerName: dockerStr[3],
+			CpuPre:        dockerStr[1],
+			MemUsageLimit: dockerStr[2],
+		})
+
+	}
+	defer func() {
+		session.Close()
+	}()
+	return
+}
+
+func getBaseInfo(client *ssh.Client) (ret BaseInfo, err error) {
+	var session *ssh.Session
+	var res []byte
+	session, err = client.NewSession()
+	defer func() {
+		session.Close()
+	}()
+	if err != nil {
+		err = errors.Wrap(err, "New session error")
+		return
+	}
+	var stdErr bytes.Buffer
+	session.Stderr = &stdErr
+	stdout, _ := session.StdoutPipe()
+	err = session.Run(TopCMD)
+	if err != nil {
+		err = errors.Wrap(err, "Cmd error")
+		return
+	}
+	res, err = ioutil.ReadAll(stdout)
+	if err != nil {
+		return
+	}
+	data := strings.Split(string(res), "\n")
+	if len(data) < 3 {
+		return
+	}
+	upReg := regexp.MustCompile(`up\s(\d+\s\w+),`)
+	userReg := regexp.MustCompile(`\s+(\d+)\suser,\s+load`)
+	memUsReg := regexp.MustCompile(`KiB\s+Mem\s+:\s+(\d+)\stotal,\s+(\d+)\sfree,\s+(\d+)\sused,\s+(\d+)\sbuff`)
+	cpuReg := regexp.MustCompile(`Cpu\(s\):\s+(\d+.\d+)\sus,\s+(\d+.\d+)\ssy,.*?,.*?,\s+(\d+.\d+).*?,.*?,.*?,\s+(\d+.\d+)\s+st`)
+
+	for i, line := range data[:4] {
+		if i == 0 {
+			up := upReg.FindStringSubmatch(line)
+			if len(up) != 2 {
+				continue
+			}
+			ret.Uptime = up[1]
+			user := userReg.FindStringSubmatch(line)
+			userCount, err := strconv.Atoi(user[1])
+			if err != nil {
+				continue
+			}
+			ret.OnlineUser = userCount
+		}
+		if i == 2 {
+			cpuRes := cpuReg.FindStringSubmatch(line)
+			if len(cpuRes) != 5 {
+				log.Info(cpuRes)
+				continue
+			}
+			for i, item := range cpuRes[1:] {
+				cInt, err := strconv.ParseFloat(item, 64)
+				if err != nil {
+					continue
+				}
+				switch i {
+				case 0:
+					ret.UserUse = cInt
+				case 1:
+					ret.SysUse = cInt
+				case 2:
+					ret.IOWait = cInt
+				case 3:
+					ret.Steal = cInt
+				}
+			}
+		}
+		if i == 3 {
+			memUseArray := memUsReg.FindStringSubmatch(line)
+			if len(memUseArray) != 5 {
+				continue
+			}
+			for i, item := range memUseArray {
+				intC, err := strconv.Atoi(item)
+				if err != nil {
+					continue
+				}
+				switch i {
+				case 0:
+					ret.Total = int64(intC)
+				case 1:
+					ret.Free = int64(intC)
+				case 2:
+					ret.Used = int64(intC)
+				case 3:
+					ret.Cache = int64(intC)
+
+				}
+			}
+		}
+	}
+	return
 }
 
 func MonitorEndpoint(c echo.Context) (err error) {
@@ -52,10 +219,10 @@ func MonitorEndpoint(c echo.Context) (err error) {
 	defer func() {
 		ws.Close()
 	}()
-	//var returnData MonitorInfo
+	var returnData MonitorInfo
 	switch model.AssetProto(asset.Protocol) {
 	case model.SSH:
-		nextTerminal, err := term.NewSshClient(asset.IP, asset.Port, asset.Username, asset.Password,
+		terminal, err := term.NewSshClient(asset.IP, asset.Port, asset.Username, asset.Password,
 			asset.PrivateKey, asset.Passphrase)
 		if err != nil {
 			log.Error(err)
@@ -73,33 +240,24 @@ func MonitorEndpoint(c echo.Context) (err error) {
 			if string(msg) != "status" {
 				break
 			}
-			session, err := nextTerminal.NewSession()
+			dockerInfo, err := GetDockerInfo(terminal)
 			if err != nil {
-				return WriteMessage(ws, FailMessage())
+				log.Error(errors.New("format error"))
+				return WriteMessage(ws, NewMessage(Closed, "Get status error"))
 			}
-			stdOut, _ := session.StdoutPipe()
-			//stdIn, _ := session.StdinPipe()
-			err = session.Run(DockerCMD)
+			returnData.Docker = dockerInfo
+			base, err := getBaseInfo(terminal)
 			if err != nil {
-				log.Error(err)
-				return WriteMessage(ws, FailMessage())
+				log.Error(err, "getBaseInfo error")
+				return WriteMessage(ws, NewMessage(Closed, "Get status error"))
 			}
-			res, _ := ioutil.ReadAll(stdOut)
-			WriteByteMessage(ws, res)
-			//for _, line := range strings.Split(string(res), "\n")[1:] {
-
-			//dockerStr := strings.Split(line, "\\t")
-			//if len(dockerStr) != 4 {
-			//	log.Error(errors.New("format error"))
-			//	return WriteMessage(ws, NewMessage(Closed, "format error"))
-			//}
-			//returnData.Docker = append(returnData.Docker, DockerInfo{
-			//	ContainerID:   dockerStr[0],
-			//	ContainerName: dockerStr[3],
-			//	CpuPre:        dockerStr[1],
-			//	MemUsageLimit: dockerStr[2],
-			//})
-			//}
+			returnData.BaseInfo = base
+			data, err := json.Marshal(returnData)
+			if err != nil {
+				log.Error(errors.New("Marshal error"))
+				return WriteMessage(ws, NewMessage(Closed, "monitor error"))
+			}
+			WriteByteMessage(ws, data)
 		}
 	}
 	return
