03b59d6a2fa549be391e3490985fe9bd60b7d448
dushixiang <helloworld1024@foxmail.com>
2022-04-20 22:03:45 +0800

    修复「接入网关」+「guacd」无法使用监控和文件管理功能的问题

--- server/global/gateway/tunnel.go
+++ server/global/gateway/tunnel.go
@@ -24,33 +24,36 @@ type Tunnel struct {
 
 func (r *Tunnel) Open() {
 	localAddr := fmt.Sprintf("%s:%d", r.LocalHost, r.LocalPort)
+	for {
+		select {
+		case <-r.ctx.Done():
+			_ = r.listener.Close()
+			log.Debugf("SSH 隧道 %v 关闭", localAddr)
+			return
+		default:
+			log.Debugf("等待客户端访问 %v", localAddr)
+			localConn, err := r.listener.Accept()
+			if err != nil {
+				log.Debugf("接受连接失败 %v", err.Error())
+				continue
+			}
 
-	go func() {
-		<-r.ctx.Done()
-		_ = r.listener.Close()
-		log.Debugf("SSH 隧道 %v 关闭", localAddr)
-	}()
-	log.Debugf("等待客户端访问 %v", localAddr)
-	localConn, err := r.listener.Accept()
-	if err != nil {
-		r.err = err
-		return
+			log.Debugf("客户端 %v 连接至 %v", localConn.RemoteAddr().String(), localAddr)
+			remoteAddr := fmt.Sprintf("%s:%d", r.RemoteHost, r.RemotePort)
+			log.Debugf("连接远程主机 %v ...", remoteAddr)
+			remoteConn, err := r.Gateway.SshClient.Dial("tcp", remoteAddr)
+			if err != nil {
+				log.Debugf("连接远程主机 %v 失败", remoteAddr)
+				r.err = err
+				return
+			}
+
+			log.Debugf("连接远程主机 %v 成功", remoteAddr)
+			go copyConn(localConn, remoteConn)
+			go copyConn(remoteConn, localConn)
+			log.Debugf("转发数据 [%v]->[%v]", localAddr, remoteAddr)
+		}
 	}
-
-	log.Debugf("客户端 %v 连接至 %v", localConn.RemoteAddr().String(), localAddr)
-	remoteAddr := fmt.Sprintf("%s:%d", r.RemoteHost, r.RemotePort)
-	log.Debugf("连接远程主机 %v ...", remoteAddr)
-	remoteConn, err := r.Gateway.SshClient.Dial("tcp", remoteAddr)
-	if err != nil {
-		log.Debugf("连接远程主机 %v 失败", remoteAddr)
-		r.err = err
-		return
-	}
-
-	log.Debugf("连接远程主机 %v 成功", remoteAddr)
-	go copyConn(localConn, remoteConn)
-	go copyConn(remoteConn, localConn)
-	log.Debugf("转发数据 [%v]->[%v]", localAddr, remoteAddr)
 }
 
 func (r Tunnel) Close() {
