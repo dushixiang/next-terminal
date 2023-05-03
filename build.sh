c2b76717af1dead354fef56f385a2e5adbdab72d
dushixiang <helloworld1024@foxmail.com>
2023-05-03 16:48:53 +0800

    将自动发版修改为独立的Action

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ echo "build web success"
 
 echo "build api..."
 cd ..
+go mod tidy
 go env;CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-s -w' -o next-terminal main.go
 upx next-terminal
 
