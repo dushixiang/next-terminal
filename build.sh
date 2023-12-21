787e959487e35705157fa630d88d7b1114533232
Kevin <62093609+Kevin5010@users.noreply.github.com>
2023-12-22 01:18:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,16 +8,5 @@ rm -rf server/resource/build
 echo "clean build history"
 
 echo "build web..."
-cd web || exit
-yarn build || exit
-cp -r build ../server/resource/
-echo "build web success"
-
-echo "build api..."
-cd ..
-go mod tidy
-go env;CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-s -w' -o next-terminal main.go
-upx next-terminal
-
-rm -rf server/resource/build
-echo "build api success"
+cd web
+npm run build
